3742331c26cd9a21130fc5a79f7861ca761e1c27
Abdulfattox <vwhoami@yahoo.com>
2023-06-06 17:12:22 +0500

    Headrer done

--- app/src/App.js
+++ app/src/App.js
@@ -2,14 +2,18 @@ import { AnimatePresence } from "framer-motion";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Main from "./components/Main/Main";
+import HeaderImg from "./assets/images/header-img.png";
 
 function App() {
   return (
     <AnimatePresence>
       <div className="h-full w-full flex flex-col">
         <Navbar />
-        <header className="container">
-
+        <header className="container bg-gray-950 flex justify-evenly items-center rounded-3xl my-7 shadow-xl">
+          <h1 className="text-white text-4xl font-bold">
+            Аксессуары для <br /> Iphone 13 Pro Max
+          </h1>
+          <img src={HeaderImg} alt="iphone-img" className="-mt-10" />
         </header>
         <Main />
         <Footer />
